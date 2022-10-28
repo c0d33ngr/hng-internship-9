4085ca2377eea4d02d110e46cf363f9257cebe7a
Jeffrey Whewhetu <jeffwhewhetu@gmail.com>
2022-10-28 22:17:58 +0100

    api to display user info from url endpoint

---
+++ task1/api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class UserInfo(models.Model):
+    username = models.CharField(max_length=20)
+    backend = models.BooleanField()
+    age = models.IntegerField()
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.username
---
+++ task1/api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import UserInfoView
+
+urlpatterns = [
+    path("userinfo/", UserInfoView.as_view()),
+]
