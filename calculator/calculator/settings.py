c665431a5a37dc83d9d2cf85220aabb863600bb5
Jeffrey Whewhetu <jeffwhewhetu@gmail.com>
2022-11-07 11:58:28 +0100

    Update repo for heroku deployment

--- calculator/calculator/settings.py
+++ calculator/calculator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku, dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,11 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     #third party app
     "rest_framework",
     "corsheaders",
-    
+
     #custom app
     "api.apps.ApiConfig",
 
@@ -124,7 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = BASE_DIR / "static"
 
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
